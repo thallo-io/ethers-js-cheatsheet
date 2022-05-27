7d45d99116d87f89e431ddddd122fdc9ee3b9a78
Derked <nazek8@gmail.com>
2022-05-27 09:18:55 -0400

    final edits

--- scripts/readOpenSea.ts
+++ scripts/readOpenSea.ts
@@ -1,16 +1,19 @@
 import { ethers } from "hardhat";
-import WyvernProtocol from "../wyvernProtocol.json";
+import wyvern from "../wyvernProtocol.json";
+import { WyvernProtocol } from "../types-abi";
 
 const readOS = async () => {
-  const [owner] = await ethers.getSigners();
+  const signers = await ethers.getSigners();
   const osAddress = "0x7f268357a8c2552623316e2562d90e642bb538e5";
   const wyvernExchange = new ethers.Contract(
     osAddress,
-    WyvernProtocol.abi,
-    owner
-  );
+    wyvern.abi,
+    signers[0]
+  ) as WyvernProtocol;
   const codename = await wyvernExchange.codename();
   console.log(codename); //should be "Bulk Smash"
+
+  const diffContract = wyvernExchange.connect(signers[1]);
 };
 
 readOS().catch((e) => {
